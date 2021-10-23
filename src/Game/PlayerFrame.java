fbe2b92ecb842b26482d43c5dfdf6b6c9b82d1e8
Queraxe <adrian.jonkov@gmail.com>
2021-10-23 19:57:28 +0200

    komplett neu alles

---
+++ src/Game/PlayerFrame.java
@@ -0,0 +1,238 @@
+package Game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.*;
+import java.net.Socket;
+
+public class PlayerFrame {
+
+    JFrame frame;
+    String serverIP = "92.75.167.95";
+    int port = 9933;
+    private Socket socket;
+    int playerID;
+    int width = 600;
+    int height = 400;
+    private PlayerSprite me, enemy;
+    private DrawingComponent dc;
+    private Timer animationTimer;
+    private boolean up, down, left, right;
+    private ReadFromServer rfsRunnable;
+    private WriteToServer wtsRunnable;
+
+    public PlayerFrame(){
+        System.out.println("=== PLAYER ===");
+        up = false;
+        down = false;
+        left = false;
+        right = false;
+    }
+
+    public void GUI(){
+        frame = new JFrame();
+        frame.setSize(width, height);
+        frame.setTitle("Player #" + playerID);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        dc = new DrawingComponent();
+        frame.add(dc);
+
+        frame.setVisible(true);
+
+        setUpAnimationTimer();
+        setUpKeyListener();
+    }
+
+    public void createPlayers(){
+        if (playerID == 1){
+            me = new PlayerSprite(100,200, 50, Color.blue);
+            enemy = new PlayerSprite(200,200,50, Color.red);
+        } else if (playerID == 2){
+            me = new PlayerSprite(200,200,50, Color.red);
+            enemy = new PlayerSprite(100,200, 50, Color.blue);
+        } else{ // none player
+            me = new PlayerSprite(100,200, 50, Color.blue);
+            enemy = new PlayerSprite(200,200,50, Color.red);
+        }
+    }
+
+    private void setUpAnimationTimer(){
+        int interval = 10;
+        ActionListener al = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double speed = 5;
+
+                if (up) {
+                    me.moveV(-speed);
+                } if (down){
+                    me.moveV(speed);
+                } if (left){
+                    me.moveH(-speed);
+                } if (right){
+                    me.moveH(speed);
+                }
+                dc.repaint();
+            }
+        };
+        animationTimer = new Timer(interval, al);
+        animationTimer.start();
+    }
+
+    private void setUpKeyListener(){
+        KeyListener kl = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+
+                if (keyCode == KeyEvent.VK_UP) {
+                    up = true;
+                } else if (keyCode == KeyEvent.VK_DOWN) {
+                    down = true;
+                } else if (keyCode == KeyEvent.VK_LEFT) {
+                    left = true;
+                } else if (keyCode == KeyEvent.VK_RIGHT) {
+                    right = true;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+
+                if (keyCode == KeyEvent.VK_UP) {
+                    up = false;
+                } else if (keyCode == KeyEvent.VK_DOWN) {
+                    down = false;
+                } else if (keyCode == KeyEvent.VK_LEFT) {
+                    left = false;
+                } else if (keyCode == KeyEvent.VK_RIGHT) {
+                    right = false;
+                }
+            }
+        };
+        frame.addKeyListener(kl);
+        frame.setFocusable(true);
+    }
+
+
+    public void connect(){
+        try {
+            socket = new Socket("localhost", port);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            playerID = in.readInt();
+            System.out.println("You are Player #" + playerID);
+
+            if (playerID == 1){
+                System.out.println("Waiting for Player #2");
+            }
+            rfsRunnable = new ReadFromServer(in);
+            wtsRunnable = new WriteToServer(out);
+            rfsRunnable.waitForStartMsg();
+
+        } catch (IOException ex){
+            System.out.println("IOException from connect()");
+        }
+    }
+
+    private class DrawingComponent extends JComponent{
+        protected void paintComponent(Graphics g){
+            Graphics2D g2d = (Graphics2D) g;
+            me.drawSprite(g2d);
+            enemy.drawSprite(g2d);
+        }
+    }
+
+
+    private class ReadFromServer implements Runnable{
+        private DataInputStream dataIn;
+
+        public ReadFromServer(DataInputStream in){
+            dataIn = in;
+            System.out.println("RFS Runnable created");
+        }
+
+        @Override
+        public void run() {
+            System.out.println("run");
+            try {
+                while (true) {
+                    if (enemy != null) {
+                        System.out.println("in");
+                        enemy.setX(dataIn.readDouble());
+                        enemy.setY(dataIn.readDouble());
+                    }
+                }
+            } catch (IOException ex) {
+                System.out.println("IOException from RFS run()");
+            }
+        }
+        public void waitForStartMsg() {
+            try {
+                String startMsg = dataIn.readUTF();
+                System.out.println("Message from server: " + startMsg);
+
+                Thread readThread = new Thread(rfsRunnable);
+                Thread writeThread = new Thread(wtsRunnable);
+                readThread.start();
+                writeThread.start();
+
+            } catch (IOException ex){
+                System.out.println("IOException from waitForStartMsg()");
+            }
+        }
+    }
+    private class WriteToServer implements Runnable{
+        private DataOutputStream dataOut;
+
+        public WriteToServer(DataOutputStream out){
+            dataOut = out;
+            System.out.println("WTS Runnable created");
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    if (me != null) {
+                        dataOut.writeDouble(me.getX());
+                        dataOut.writeDouble(me.getY());
+                        dataOut.flush();
+                    }
+
+                    try {
+                        Thread.sleep(25);
+                    } catch (InterruptedException ex) {
+                        System.out.println("InterruptedException from WTS run()");
+                    }
+                }
+
+            } catch (IOException ex) {
+                System.out.println("IOException from WTS run()");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PlayerFrame pf = new PlayerFrame();
+
+        pf.createPlayers();
+        pf.connect();
+
+        pf.GUI();
+
+    }
+
+}
---
+++ src/Server/GameServer.java
@@ -0,0 +1,165 @@
+package Server;
+
+import java.io.*;
+import java.net.*;
+
+public class GameServer {
+
+    private ServerSocket ss;
+    private int numPort = 9933;
+    private int numPlayers;
+    private int maxPlayers;
+
+    private Socket p1Socket;
+    private Socket p2Socket;
+    private ReadFromClient p1ReadRunnable, p2ReadRunnable;
+    private WriteToClient p1WriteRunnable, p2WriteRunnable;
+
+    public static double p1x,p1y,p2x,p2y;
+
+
+    public GameServer(){
+        System.out.println("=== GAME SERVER ===");
+        numPlayers = 0;
+        maxPlayers = 2;
+
+        p1x = 100;
+        p1y = 200;
+        p2x = 200;
+        p2y = 200;
+
+        try {
+            ss = new ServerSocket(numPort);
+        } catch (IOException ex) {
+            System.out.println("IOException from GameServer constructor");
+        }
+    }
+
+    public void acceptClients(){
+
+        while (numPlayers < maxPlayers) {
+            try {
+                Socket s = ss.accept();
+                DataInputStream in = new DataInputStream(s.getInputStream());
+                DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+                numPlayers++;
+                out.writeInt(numPlayers);
+                System.out.println("Player #" + numPlayers + " connected");
+
+                ReadFromClient rfc = new ReadFromClient(numPlayers, in);
+                WriteToClient wtc = new WriteToClient(numPlayers, out);
+
+                if (numPlayers == 1){
+                    p1Socket = s;
+                    p1ReadRunnable = rfc;
+                    p1WriteRunnable = wtc;
+                }  else {
+                    p2Socket = s;
+                    p2ReadRunnable = rfc;
+                    p2WriteRunnable = wtc;
+                    p1WriteRunnable.sendStartMsg();
+                    p2WriteRunnable.sendStartMsg();
+
+                    Thread readThread1 = new Thread(p1ReadRunnable);
+                    Thread readThread2 = new Thread(p2ReadRunnable);
+                    readThread1.start();
+                    readThread2.start();
+                    Thread writeThread1 = new Thread(p1WriteRunnable);
+                    Thread writeThread2 = new Thread(p2WriteRunnable);
+                    writeThread1.start();
+                    writeThread2.start();
+                }
+
+            } catch (IOException e) {
+                System.out.println("IOException from acceptClient()");
+            }
+        }
+        System.out.println("--The GAME begins!--");
+    }
+
+    public void end(){
+        numPlayers = 0;
+
+    }
+
+
+    private static class ReadFromClient implements Runnable{
+
+        private int playerID;
+        private DataInputStream dataIn;
+
+        public ReadFromClient(int pid, DataInputStream in){
+            playerID = pid;
+            dataIn = in;
+            System.out.println("RFC" + playerID + "Runnable created");
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    if (playerID == 1) {
+                        p1x = dataIn.readDouble();
+                        p1y = dataIn.readDouble();
+                    } else {
+                        p2x = dataIn.readDouble();
+                        p2y = dataIn.readDouble();
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("IOException from RFC run()");
+            }
+        }
+    }
+    private static class WriteToClient implements Runnable{
+
+        private int playerID;
+        private DataOutputStream dataOut;
+
+        public WriteToClient(int pid, DataOutputStream in){
+            playerID = pid;
+            dataOut = in;
+            System.out.println("WTC" + playerID + "Runnable created");
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    if (playerID == 1) {
+                        dataOut.writeDouble(p2x);
+                        dataOut.writeDouble(p2y);
+                    } else {
+                        dataOut.writeDouble(p1x);
+                        dataOut.writeDouble(p1y);
+                    }
+                    dataOut.flush();
+                    try {
+                        Thread.sleep(25);
+                    } catch (InterruptedException ex) {
+                        System.out.println("InterruptedException from WTC run()");
+                    }
+                }
+            } catch (IOException ex) {
+                System.out.println("IOException from WTC run()");
+            }
+        }
+
+        public void sendStartMsg(){
+            try {
+                dataOut.writeUTF("--- GO! ---");
+            } catch (IOException ex) {
+                System.out.println("IOException from sendStartMsg()");
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        GameServer gs = new GameServer();
+        gs.acceptClients();
+
+        gs.end();
+    }
+}
